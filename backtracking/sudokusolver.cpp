044a983c5e58521e9800987c7aad8d037f6863c5
Oliver Kidd <91744164+NoobHacker87@users.noreply.github.com>
2021-10-01 21:23:39 +0530

    Update sudokusolver.cpp

--- backtracking/sudokusolver.cpp
+++ backtracking/sudokusolver.cpp
@@ -1,16 +1,45 @@
+/**
+ * @file
+ * @brief [Sudoku Solver](https://en.wikipedia.org/wiki/Sudoku) algorithm.
+ *
+ * @details
+ * Sudoku (数独, sūdoku, digit-single) (/suːˈdoʊkuː/, /-ˈdɒk-/, /sə-/, originally called
+ * Number Place) is a logic-based, combinatorial number-placement puzzle. 
+ * In classic sudoku, the objective is to fill a 9×9 grid with digits so that each column,
+ * each row, and each of the nine 3×3 subgrids that compose the grid (also called "boxes", "blocks", or "regions")
+ * contain all of the digits from 1 to 9. The puzzle setter provides a
+ * partially completed grid, which for a well-posed puzzle has a single solution.
+ 
+ */
 #include <iostream>
 #include <array>
 
+/**
+ * @namespace backtracking
+ * @brief Backtracking algorithms
+ */
 namespace backtracking {
- 
+    /**
+     * Checks if it's possible to place a number 'no'
+     * @tparam V number of vertices in the array
+     * @param mat matrix where numbers are saved
+     * @param i current index in rows
+     * @param j current index in columns
+     * @param no number to be added in matrix
+     * @param n number of times loop will run
+     * @returns `true` if 'mat' is different from 'no'
+     * @returns `false` if 'mat' equals to 'no'
+     */
     template <size_t V>
     bool isPossible(const std::array <std::array <int, V>, V> &mat, int i, int j, int no, int n) {
+        /// 'no' shouldn't be present in either row i or column j
         for (int x = 0; x < n; x++) {
             if (mat[x][j] == no || mat[i][x] == no) {
                 return false;
             }
         }
 
+        /// 'no' shouldn't be present in the 3*3 subgrid
         int sx = (i / 3) * 3;
         int sy = (j / 3) * 3;
 
@@ -24,7 +53,14 @@ namespace backtracking {
 
         return true;
     }
-   
+    /**
+     * Utility function to print matrix
+     * @tparam V number of vertices in array
+     * @param mat matrix where numbers are saved
+     * @param starting_mat copy of mat, required by printMat for highlighting the differences
+     * @param n number of times loop will run
+     * @return void
+     */
     template <size_t V>
     void printMat(const std::array <std::array <int, V>, V> &mat, const std::array <std::array <int, V>, V> &starting_mat, int n) {
         for (int i = 0; i < n; i++) {
@@ -45,35 +81,57 @@ namespace backtracking {
         }
     }
 
-    
+    /**
+     * Sudoku algorithm
+     * @tparam V number of vertices in array
+     * @param mat matrix where numbers are saved
+     * @param starting_mat copy of mat, required by printMat for highlighting the differences
+     * @param i current index in rows
+     * @param j current index in columns
+     * @returns `true` if 'no' was placed
+     * @returns `false` if 'no' was not placed
+     */
     template <size_t V>
     bool solveSudoku(std::array <std::array <int, V>, V> &mat, const std::array <std::array <int, V>, V> &starting_mat, int i, int j) {
+        /// Base Case
         if (i == 9) {
+            /// Solved for 9 rows already
             backtracking::printMat<V>(mat, starting_mat, 9);
             return true;
         }
 
+        /// Crossed the last  Cell in the row
         if (j == 9) {
             return backtracking::solveSudoku<V>(mat, starting_mat, i + 1, 0);
         }
 
+        /// Blue Cell - Skip
         if (mat[i][j] != 0) {
             return backtracking::solveSudoku<V>(mat, starting_mat, i, j + 1);
         }
+        /// White Cell
+        /// Try to place every possible no
         for (int no = 1; no <= 9; no++) {
             if (backtracking::isPossible<V>(mat, i, j, no, 9)) {
+                /// Place the 'no' - assuming a solution will exist
                 mat[i][j] = no;
                 bool solution_found = backtracking::solveSudoku<V>(mat, starting_mat, i, j + 1);
                 if (solution_found) {
                     return true;
                 }
+                /// Couldn't find a solution
+                /// loop will place the next no.
             }
         }
+        /// Solution couldn't be found for any of the numbers provided
         mat[i][j] = 0;
         return false;
     }
 } // namespace backtracking
 
+/**
+ * Main function
+ */
 int main() {
     const int V = 9;
     std::array <std::array <int, V>, V> mat = { 
